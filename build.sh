f8ed40fadd5b38587209721c826494830e0de036
ecky-l <ecky-l@users.noreply.github.com>
2020-05-11 19:44:54 +0200

    envsubst to substitute variables in yaml file

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+errorExit() {
+    echo "$1"
+    exit 1
+}
+
+. ./env || errorExit "env file not found. Copy from env.sample and modify to your needs"
+
 IGNITION=kube-master
 
 RUN_CMD=podman
 command -v ${RUN_CMD} 2 > /dev/null || RUN_CMD=docker
 
-${RUN_CMD} run -i --rm quay.io/coreos/fcct:release --pretty --strict < ${IGNITION}.yaml > ${IGNITION}.ign
+cat ${IGNITION}.yaml | envsubst | ${RUN_CMD} run -i --rm quay.io/coreos/fcct:release --strict > ${IGNITION}.ign
 ${RUN_CMD} run --rm -i quay.io/coreos/ignition-validate - < ${IGNITION}.ign
 
 # run a webserver to serve the thing
