3eb6fb4160c6bced3cf13f1f9a89dcbec37da3bc
ecky-l <ecky-l@users.noreply.github.com>
2020-05-11 23:50:11 +0200

    more envsubst expansion and optional network setup

--- build.sh
+++ build.sh
@@ -5,15 +5,20 @@ errorExit() {
     exit 1
 }
 
-. ./env || errorExit "env file not found. Copy from env.sample and modify to your needs"
+TYPE=$1
+shift
 
-IGNITION=kube-master
+. ./env-"${TYPE}" || errorExit "env-${TYPE} file not found. Copy from env.sample and modify to your needs"
+
+for a in "$@"; do
+    export $a
+done
 
 RUN_CMD=podman
 command -v ${RUN_CMD} 2 > /dev/null || RUN_CMD=docker
 
-cat ${IGNITION}.yaml | envsubst | ${RUN_CMD} run -i --rm quay.io/coreos/fcct:release --strict > ${IGNITION}.ign
-${RUN_CMD} run --rm -i quay.io/coreos/ignition-validate - < ${IGNITION}.ign
+cat kube.yaml | envsubst | ${RUN_CMD} run -i --rm quay.io/coreos/fcct:release --strict > ${TYPE}.ign
+${RUN_CMD} run --rm -i quay.io/coreos/ignition-validate - < ${TYPE}.ign
 
 # run a webserver to serve the thing
 #python -m http.server
