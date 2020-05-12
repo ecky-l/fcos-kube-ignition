ebbcf3b88153fcf6ea23452d94cde57c39f74fb9
ecky-l <ecky-l@users.noreply.github.com>
2020-05-12 22:20:07 +0200

    fixed envsubst substitution of FCOS_ADVERTISE_IP in network-config.yaml. build.sh documentation comments and output

--- build.sh
+++ build.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
 errorExit() {
-    echo "$1"
+    for s in "$@"; do
+        echo "$s"
+    done
     exit 1
 }
 
+test -z "$@" && errorExit "Usage: ./build.sh <name> [VARIABLE1_OVERWRITE=value ...]." \
+                          "   <name> - A name for which the ./env-<name> file exists with variables like in env.sample" \
+                          "            It will result in a corresponding <name>.ign file"
 TYPE=$1
 shift
 
-. ./env-"${TYPE}" || errorExit "env-${TYPE} file not found. Copy from env.sample and modify to your needs"
+. ./env-"${TYPE}" >/dev/null 2>&1 || errorExit "./env-${TYPE}: No such file. Copy ./env.sample to ./env-${TYPE} and edit it to your needs"
 
 for a in "$@"; do
     export $a
 done
 
+if test -f ./network-config.yaml; then
+    export FCOS_NETWORK_CONFIG="$(cat network-config.yaml | envsubst)"
+else
+    echo "[W] network-config.yaml not found. No static network configuration will be performed."
+    echo "[W] If that is not your intention, copy ./network-config-sample.yaml to ./network-config.yaml and edit it to your needs."
+fi
+
+
+echo "Building ${TYPE}.ign ..."
 RUN_CMD=podman
 command -v ${RUN_CMD} 2 > /dev/null || RUN_CMD=docker
 
 cat kube.yaml | envsubst | ${RUN_CMD} run -i --rm quay.io/coreos/fcct:release --strict > ${TYPE}.ign
 ${RUN_CMD} run --rm -i quay.io/coreos/ignition-validate - < ${TYPE}.ign
+
+echo "Done. You can now expose ${TYPE}.ign for download (consider 'python -m http.server') or use it otherwise to bootstrap a Fedora CoreOS instance"
 
 # run a webserver to serve the thing
 #python -m http.server
