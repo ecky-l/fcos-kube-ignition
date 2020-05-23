e8f97358a8e078910885a038143aa084e301b675
ecky-l <ecky-l@users.noreply.github.com>
2020-05-23 11:48:25 +0200

    matchbox config added to ignition

--- modules/bootstrapper/main.tf
+++ modules/bootstrapper/main.tf
@@ -1,9 +1,9 @@
 
 data "ct_config" "bootstrapper-ignition" {
-  strict   = true
+  strict = true
   pretty_print = true
   content = templatefile("${path.module}/templates/bootstrapper.yaml", {
-    host_name           = var.host_name
+    host_name = var.host_name
     ssh_authorized_keys = var.ssh_authorized_keys
   })
   snippets = [
@@ -16,6 +16,11 @@ data "ct_config" "bootstrapper-ignition" {
       dhcpd_dns = var.dhcpd_dns
       dhcpd_interface = var.dhcpd_interface
     }),
-    file("${path.module}/templates/snippets/tftpd.yaml")
+    file("${path.module}/templates/snippets/tftpd.yaml"),
+    templatefile("${path.module}/templates/snippets/matchbox.yaml", {
+      ca_cert = tls_self_signed_cert.matchbox-ca.cert_pem,
+      server_cert = tls_locally_signed_cert.matchbox-server.cert_pem,
+      server_key = tls_private_key.matchbox-server.private_key_pem
+    })
   ]
 }
--- modules/bootstrapper/tls-matchbox.tf
+++ modules/bootstrapper/tls-matchbox.tf
@@ -61,7 +61,7 @@ resource "tls_locally_signed_cert" "matchbox-server" {
     "server_auth",
     "client_auth",
   ]
-  ca_cert_pem = tls_self_signed_cert.matchbox-ca.private_key_pem
+  ca_cert_pem = tls_self_signed_cert.matchbox-ca.cert_pem
   ca_key_algorithm = tls_self_signed_cert.matchbox-ca.key_algorithm
   ca_private_key_pem = tls_private_key.matchbox-ca.private_key_pem
   cert_request_pem = tls_cert_request.matchbox-server.cert_request_pem
