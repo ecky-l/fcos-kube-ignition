b6dafd3685955eda4ec9625c0a690e60f951f6b9
ecky-l <ecky-l@users.noreply.github.com>
2020-05-27 09:06:11 +0200

    documented necessary presence of .sig signature for image download. Needs to go to a README though

--- modules/fcos-kube-matchbox/main.tf
+++ modules/fcos-kube-matchbox/main.tf
@@ -11,6 +11,9 @@ locals {
     "ignition.platform.id=metal",
     "coreos.inst.install_dev=${var.install_disk}",
     "coreos.inst.stream=${var.os_stream}",
+    # for the next line to work, the .raw.xz must be downloaded to the appropriate place.
+    # !!! AND the .raw.xz.sig file must be downloaded from the same location and placed next to the .raw.xz file !!!
+    # If the .sig file is not present, there will be a hang-screen during PXE boot and no message why.
     "coreos.inst.image_url=${var.matchbox_http_endpoint}/assets/fcos/${var.os_version}/fedora-coreos-${var.os_version}-metal.x86_64.raw.xz",
     "coreos.inst.ignition_url=${var.matchbox_http_endpoint}/ignition?uuid=$${uuid}&mac=$${mac:hexhyp}",
   ]
